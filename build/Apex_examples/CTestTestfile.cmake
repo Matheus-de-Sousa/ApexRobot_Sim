1aecbd12e32963246618ee0d2d0ce7286f50446d
Matheus de Sousa <matheus.sousa3089@gmail.com>
2025-06-08 22:19:50 -0300

    Delete build directory

--- build/Apex_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matheus/Documents/Apex_ws/src/Apex_examples
-# Build directory: /home/matheus/Documents/Apex_ws/build/Apex_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/apex_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matheus/Documents/Apex_ws/src/apex_description
-# Build directory: /home/matheus/Documents/Apex_ws/build/apex_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
